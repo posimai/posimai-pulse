2750ba563def74f9eecdbef2021ee2849f11734e
posimai <posimai.project@gmail.com>
2026-03-19 22:20:49 +0900

    fix: bump SW cache version to invalidate icon cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'posimai-pulse-v1';
+const CACHE = 'posimai-pulse-v2';
 const STATIC = ['/', '/index.html', '/manifest.json', '/logo.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(STATIC)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
