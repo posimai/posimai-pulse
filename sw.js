8ee8b9b497019a92f1aad09dda6839b70db7873f
posimai <posimai.project@gmail.com>
2026-03-27 12:27:27 +0900

    fix: add .catch() to SW activate event

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 const CACHE = 'posimai-pulse-v3';
 const STATIC = ['/', '/index.html', '/manifest.json', '/logo.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(STATIC)).then(() => self.skipWaiting())); });
-self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()).catch(err => console.warn('[SW] activate error', err))); });
 const ORIGIN = self.location.origin;
 self.addEventListener('fetch', e => {
     if (e.request.method !== 'GET') return;
